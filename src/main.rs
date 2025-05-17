5ff724761bbb4b5726ba58b1f9f8fbb5b53a6675
spineda2019 <spineda.wpi.alum@gmail.com>
2025-05-17 11:38:04 -0400

    stub hellow world example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+slint::slint! {
+    export component MainWindow inherits Window {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let main_window: MainWindow = match MainWindow::new() {
+        Ok(w) => w,
+        Err(_) => todo!(),
+    };
+
+    if let Err(e) = main_window.run() {
+        todo!();
+    }
 }
