921ab06d02d621a082a6cfdac203f09b0711d118
spineda2019 <spineda.wpi.alum@gmail.com>
2025-05-26 11:38:35 -0400

    add menu bar; stub TODO popups

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,16 @@ fn main() {
         Err(_) => todo!(),
     };
 
+    let todo_popup: TodoPopup = match TodoPopup::new() {
+        Ok(tp) => tp,
+        Err(_e) => todo!(),
+    };
+
+    main_window.on_show_todo_popup(move || {
+        todo_popup.run();
+        todo_popup.set_popup_visible(true);
+    });
+
     if let Err(_e) = main_window.run() {
         todo!();
     }
